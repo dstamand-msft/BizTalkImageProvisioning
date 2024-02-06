f8289953d7f1fa9dded1e7b22e9210638f0b4ce3
Hugo Girard <hugirard@microsoft.com>
2024-02-05 19:47:11 -0500

    remove none needed arguments

--- BizTalkProvisioning.ps1
+++ BizTalkProvisioning.ps1
@@ -110,8 +110,10 @@ function Install-VSStudio {
     $Destination = "$installersPath\vs_community.exe"
     $Url = "https://aka.ms/vs/16/release/vs_community.exe"
     # see https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
-    $UnattendedArgs = "--quiet --norestart --installWhileDownloading --addProductLang Fr-fr"
-
+    #$UnattendedArgs = "--quiet --norestart --installWhileDownloading"
+    $UnattendedArgs = "--quiet --norestart --installWhileDownloading"
+    #$UnattendedArgs = "--quiet --norestart --installWhileDownloading --addProductLang Fr-fr"
+  
     # Download Azure CLI installer
     Write-Host "Downloading VS Studio Community Edition"
     Invoke-WebRequest -Uri $Url -OutFile $Destination
